5ea47bae8f345a99046532568c136a8354d975fc
Pruthvisj <89569955+Pruthvisj@users.noreply.github.com>
2025-05-02 16:16:16 +0530

    added atom configuration

--- management_tickets/src/App.tsx
+++ management_tickets/src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { atom, useAtom } from 'jotai';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+const atomCount = atom(0)
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [count, setCount] = useAtom(atomCount);
+
 
   return (
     <>
@@ -18,12 +20,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count: number) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+      </div>
+      <div>
+      <h1>count: {count}</h1> 
+                <button onClick={() => setCount((prev: number) => prev + 1)}>+</button>
+                <button onClick={() => setCount((prev: number) => prev - 1)}>-</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
